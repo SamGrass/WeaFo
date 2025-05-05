b657aae9078e5ccab60aa7288f91fa550469024a
Samuele Grassetti <samuele.grassetti@overit.it>
2025-05-05 18:02:17 +0200

    Add method to get the daily average temperature

--- app/home/home.controller.js
+++ app/home/home.controller.js
@@ -1,7 +1,4 @@
 angular.module('WeaFo').controller('homeController', ['weatherService', function (weatherService) {
-    var home = this;
-    home.cityDataList = [];
-    home.cities = ["London", "Tokyo", "New York", "Paris", "Rome", "Sydney", "Cairo", "Rio de Janeiro", "Toronto", "Berlin"];
 
     class City {
         constructor(apiData) {
@@ -13,10 +10,11 @@ angular.module('WeaFo').controller('homeController', ['weatherService', function
             this.sunrise = new Date(apiData.city.sunrise * 1000);
             this.sunset = new Date(apiData.city.sunset * 1000);
 
-            this.forecastData = apiData.list.map(data => {
+            this.forecastList = apiData.list.map(data => {
                 return {
                     temp: data.main.temp,
                     tempFelt: data.main.feels_like,
+                    dailyAvgTemp: null,
                     weather: {
                         type: data.weather[0].main,
                         icon: data.weather[0].icon
@@ -34,17 +32,52 @@ angular.module('WeaFo').controller('homeController', ['weatherService', function
                 }
             });
         }
+
+        getDailyAvgTemp() {
+            if (this.forecastList) {
+                var dailyData = {};
+                this.forecastList.forEach(item => {
+                    var temp = item.temp;
+                    var dateTime = new Date(item.time);
+                    var time = dateTime.toDateString();
+
+                    if (dailyData[time]) {
+                        dailyData[time].sum += temp;
+                        dailyData[time].count += 1;
+                    } else {
+                        dailyData[time] = {
+                            sum: temp,
+                            count: 1
+                        };
+                    }
+                })
+                var dailyAvg = [];
+                for (var [key] of  Object.entries(dailyData)) {
+                    var avg = Math.round(dailyData[key].sum / dailyData[key].count);
+                    dailyAvg.push({
+                        date: key,
+                        avgTemp: avg
+                    });
+                }
+                return dailyAvg;
+            }
+        }
     }
+    var home = this;
+    home.cityDataList = [];
+    home.cities = ["London", "Tokyo", "New York", "Paris", "Rome", "Sydney", "Cairo", "Rio de Janeiro", "Toronto", "Berlin"];
 
 
     angular.forEach(home.cities, function (city) {
         weatherService.getCityFromName(city).then(function (res) {
             var cityData = new City(res.data);
+            console.log(cityData.getDailyAvgTemp());
             console.log(cityData);
+            cityData.dailyAvgTemp = cityData.getDailyAvgTemp();
             home.cityDataList.push(cityData);
+
         }).catch(function (err) {
             console.log(err.message);
         })
     })
-    console.log(home.cityDataList);
 }]);
