494a8c5284f3e91fe05c822c0de0ee4b777c7013
Samuele Grassetti <samuele.grassetti@overit.it>
2025-05-05 15:19:09 +0200

    Add a table to show popular cities data, create a City class to map the DTO, refactor functions to retrieve cityData from the name

--- app/home/home.controller.js
+++ app/home/home.controller.js
@@ -1,17 +1,50 @@
 angular.module('WeaFo').controller('homeController', ['weatherService', function (weatherService) {
     var home = this;
-    home.title = 'Home';
-    home.city = {};
+    home.cityDataList = [];
+    home.cities = ["London", "Tokyo", "New York", "Paris", "Rome", "Sydney", "Cairo", "Rio de Janeiro", "Toronto", "Berlin"];
 
-    /*weatherService.getCityFromCord(46.069, 13.232)
-    .then(function (res) {
-        home.city = res.data;
-        console.log(home.city);
-    })*/
+    class City {
+        constructor(apiData) {
+            this.name = apiData.city.name;
+            this.coord = {
+                latitude: apiData.city.coord.lat,
+                longitude: apiData.city.coord.lon,
+            }
+            this.sunrise = new Date(apiData.city.sunrise * 1000);
+            this.sunset = new Date(apiData.city.sunset * 1000);
 
-    weatherService.getCityFromName('Udine').then(function (res) {
-        home.city = res.data;
-    }).catch(function (err) {
-        console.log(err.message);
+            this.forecastData = apiData.list.map(data => {
+                return {
+                    temp: data.main.temp,
+                    tempFelt: data.main.feels_like,
+                    weather: {
+                        type: data.weather[0].main,
+                        icon: data.weather[0].icon
+                    },
+                    wind: {
+                        speed: data.wind.speed, // meter/sec
+                        direction: data.wind.deg,
+                        gust: data.wind.gust // meter/sec
+                    },
+
+                    pop: data.pop, // Probability of precipitation (0 to 1)
+                    rain: data.rain ? data.rain['3h'] : null,
+                    snow: data.snow ? data.snow['3h'] : null,
+                    time: data.dt_txt,
+                }
+            });
+        }
+    }
+
+
+    angular.forEach(home.cities, function (city) {
+        weatherService.getCityFromName(city).then(function (res) {
+            var cityData = new City(res.data);
+            console.log(cityData);
+            home.cityDataList.push(cityData);
+        }).catch(function (err) {
+            console.log(err.message);
+        })
     })
+    console.log(home.cityDataList);
 }]);
--- app/services/weather.service.js
+++ app/services/weather.service.js
@@ -1,28 +1,16 @@
 angular.module('WeaFo')
     .factory('weatherService', ['$http', function ($http) {
         var apiKey = '4cd6fd8911f6e75c9afc398da4ee0de5';
+        var baseUrl = 'https://api.openweathermap.org/';
         var weatherService = {};
-
-        weatherService.getCityFromCord = function (lat, lon) {
-            return $http({
-                method: 'GET',
-                url: `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`,
-            });
-        };
 
         weatherService.getCityFromName = function (city) {
             return $http({
                 method: 'GET',
-                url: `https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${apiKey}`
-            }).then(function (res){
-                if (res.data.length === 0) {
-                    return console.log('City not found');
-                }
-                var lat = res.data[0].lat;
-                var lon = res.data[0].lon;
-                return  weatherService.getCityFromCord(lat, lon);
+                url: `${baseUrl}data/2.5/forecast?q=${city}&units=metric&appid=${apiKey}`,
             });
-        }
+        };
+
         return weatherService;
 
     }])
