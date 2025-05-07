e15548227e80d7b1080d8742c4216c7a5c155dc0
Samuele Grassetti <samuele.grassetti@overit.it>
2025-05-07 17:56:52 +0200

    Refactor weatherService object

--- app/services/weather.service.js
+++ app/services/weather.service.js
@@ -1,20 +1,19 @@
 angular.module('WeaFo')
     .service('weatherService', ['$http', function ($http) {
-        var apiKey = '4cd6fd8911f6e75c9afc398da4ee0de5';
-        var baseUrl = 'https://api.openweathermap.org/';
-        var iconBaseUrl = 'https://openweathermap.org/img/wn/';
-        var weatherService = {};
+        const apiKey = '4cd6fd8911f6e75c9afc398da4ee0de5';
+        const baseUrl = 'https://api.openweathermap.org/';
+        const iconBaseUrl = 'https://openweathermap.org/img/wn/';
 
-        weatherService.getCityFromName = function (city) {
-            return $http({
-                method: 'GET',
-                url: `${baseUrl}data/2.5/forecast?q=${city}&units=metric&appid=${apiKey}`,
-            });
+        return weatherService = {
+            getCityFromName: function (city) {
+                return $http({
+                    method: 'GET',
+                    url: `${baseUrl}data/2.5/forecast?q=${city}&units=metric&appid=${apiKey}`,
+                });
+            },
+
+            getIconBaseUrl: function () {
+                return iconBaseUrl;
+            }
         };
-
-        weatherService.getIconBaseUrl = function () {
-            return iconBaseUrl;
-        }
-        return weatherService;
-
     }])
