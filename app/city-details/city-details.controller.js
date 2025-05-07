07d38b811a3dc37dc328ec0f02b0724445a8bcf0
Samuele Grassetti <samuele.grassetti@overit.it>
2025-05-07 18:00:15 +0200

    Add the table of the city details

--- app/city-details/city-details.controller.js
+++ app/city-details/city-details.controller.js
@@ -1,11 +1,10 @@
 angular.module('WeaFo').controller('cityDetailsController', ['$routeParams', 'weatherService', function($routeParams, weatherService) {
-    var cd = this;
+    const cd = this;
     cd.cityData = {};
 
     weatherService.getCityFromName($routeParams.city)
         .then(function (res) {
             cd.cityData = new City(res.data);
-            cd.cityData.dailyAvgTemp = cd.cityData.getDailyAvgTemp();
             console.log(cd.cityData);
         })
         .catch(function (err) {
